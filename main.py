a1d0ca88e011df5ec2f35d60cbcadcf2883e32e7
skullzado <pafcalzado@gmail.com>
2025-11-23 10:29:37 +0800

    Add logger and change to uv

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ from shot import Shot
 
 
 def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
